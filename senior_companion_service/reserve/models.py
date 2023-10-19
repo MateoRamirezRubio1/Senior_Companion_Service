ed32efe525f8ac1035e9384822f02bb9ca4d805e
pauliceron <pceronm@eafit.edu.co>
2023-10-19 15:22:00 -0500

    Book appoiment function

--- senior_companion_service/reserve/models.py
+++ senior_companion_service/reserve/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+SERVICE_CHOICES = (
+    ("Activity Accompaniment", "Activity Accompaniment"),
+    ("Housework Support", "Housework Support"),
+    ("Companionship and Conversation", "Companionship and Conversation"),
+    ("Specialized Companionship", "Specialized Companionship"),
+    )
+TIME_CHOICES = (
+    ("8:00 AM", "8:00 AM"),
+    ("8:30 AM", "8:30 AM"),
+    ("9:00 AM", "9:00 AM"),
+    ("9:30 AM", "9:30 AM"),
+    ("10:00 AM", "10:00 AM"),
+    ("10:30 AM", "10:30 AM"),
+    ("11:00 AM", "11:00 AM"),
+    ("11:30 AM", "11:30 AM"),
+    ("12:00 PM", "12:00 PM"),
+    ("12:30 PM", "12:30 PM"),
+    ("1:00 PM", "1:00 PM"),
+    ("1:30 PM", "1:30 PM"),
+    ("2:00 PM", "2:00 PM"),
+    ("2:30 PM", "2:30 PM"),
+    ("3 PM", "3 PM"),
+    ("3:30 PM", "3:30 PM"),
+    ("4 PM", "4 PM"),
+    ("4:30 PM", "4:30 PM"),
+    ("5 PM", "5 PM"),
+    ("5:30 PM", "5:30 PM"),
+    ("6 PM", "6 PM"),
+    ("6:30 PM", "6:30 PM"),
+    ("7 PM", "7 PM"),
+    ("7:30 PM", "7:30 PM"),
+)
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="Doctor care")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="3 PM")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
--- senior_companion_service/reserve/urls.py
+++ senior_companion_service/reserve/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.homeReserve, name='homeReserve'),
+    path('booking/',views.booking, name='booking'),
+    path('bookingSubmit/',views.bookingSubmit, name='bookingSubmit'),
+
 ]
